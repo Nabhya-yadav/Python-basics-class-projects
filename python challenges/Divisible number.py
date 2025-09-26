ed8adca1a9fcf68a6955c7d1c90a3bbb870edb59
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-09-26 12:50:13 +0530

    Add files via upload

---
+++ python challenges/Divisible number.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the numerator "))
+num2=int(input("Enetr the denominator "))
+
+if num1%num2==0:
+    print(num1 ,"is divisible by " , num2)
+
+else:
+    print(f"{num1} is not divisible by {num2}")
---
+++ python challenges/operater precedence.py
@@ -0,0 +1,7 @@
+a=5
+b=10
+c=15
+
+equation= a**(b-c) - (a**a + b)-c
+
+print("Result=",equation)
